fc9aea640ff061eca077a4089cd104ff701baa20
Sydney Renee <sydney@solace.ofharmony.ai>
2025-09-02 21:47:23 -0700

    chore: remove temporary ML experiment directories (utilities/, thea_code_system/, east_german/)

--- thea_code_system/models/m2bert/modern.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-M2-BERT Modern Implementation Placeholder
-Will be replaced with actual implementation
-"""
-
-class M2BertModern:
-    """Placeholder for M2-BERT modern implementation"""
-    pass
