cab3c4b6fb338dacf073bb77cbc01a83a8f94c5a
Sydney Renee <sydney@solace.ofharmony.ai>
2025-09-02 21:47:23 -0700

    chore: remove temporary ML experiment directories (utilities/, thea_code_system/, east_german/)

--- thea_code_system/models/m2bert/monarch.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Monarch Matrix Implementation Placeholder
-Will be replaced with actual implementation
-"""
-
-class MonarchMatrix:
-    """Placeholder for Monarch matrix implementation"""
-    pass
